f583a52334baac86a4f55c1ca0a187d75500a26a
Matej Lukac <matej.lukac@tul.cz>
2025-01-25 18:28:22 +0100

    Debugging

--- app/device/models/difference_model.py
+++ app/device/models/difference_model.py
@@ -1,7 +1,6 @@
 from app.device.models.device_model import Device as DeviceModel
 from app.device.service import device_service as ds
 
-
 class DeviceDifference:
     def __init__(self, nb_device: DeviceModel, zb_device: DeviceModel, differences: tuple[list[str],list[str]]) -> None:
         self.nb_device: DeviceModel = nb_device
@@ -11,14 +10,14 @@ class DeviceDifference:
     def __str__(self) -> str:
         return f"{self.nb_device} {self.zb_device} {self.differences}"
 
-    def print_differences(self) -> str:
-        txt_builder: str = ""
-        txt_builder += f"Netbox device: {ds.print_device(self.nb_device)}\n"
-        txt_builder += f"Zabbix device: {ds.print_device(self.zb_device.name)}\n"
-        txt_builder += "Differences:\n"
-        for difference in self.differences[0]:
-            txt_builder += f"  {difference}\n"
-        txt_builder += "Similarities:\n"
-        for similarity in self.differences[1]:
-            txt_builder += f"  {similarity}\n"
-        return txt_builder
+def print_differences(nb_device: DeviceModel, zb_device: DeviceModel, differences: tuple[list[str],list[str]]) -> str:
+    txt_builder: str = ""
+    txt_builder += f"Netbox device: {ds.print_device(nb_device)}\n"
+    txt_builder += f"Zabbix device: {ds.print_device(zb_device)}\n"
+    txt_builder += "Differences:\n"
+    for difference in differences[0]:
+        txt_builder += f"  {difference}\n"
+    txt_builder += "Similarities:\n"
+    for similarity in differences[1]:
+        txt_builder += f"  {similarity}\n"
+    return txt_builder
