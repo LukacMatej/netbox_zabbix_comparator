795585475318e6548b823d519564d7500f14c750
Matej Lukac <matej.lukac@tul.cz>
2025-01-25 17:14:47 +0100

    Sjednoceni status

--- app/device/models/device_model.py
+++ app/device/models/device_model.py
@@ -7,15 +7,19 @@ class Device:
     description: str = ""
     templates: str = ""
     status: str = ""
-    
+            
     def __init__(self, name, interfaces, hostgroup, description, templates, status) -> None:
         self.name = name
         self.interfaces = interfaces
         self.hostgroup = hostgroup
         self.description = description
         self.templates = templates
-        self.status = status
+        self.status = normalize_status(status)
         
-    
     def __str__(self) -> str:
         return f"{self.name} {self.interfaces} {self.hostgroup} {self.description} {self.templates} {self.status}"
+
+def normalize_status(status) -> str:
+    if status == 0 or status == "Active" or status == "0":
+        return "Active"
+    return "Inactive"
