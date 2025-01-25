daa606fb0184e317a05d6361e428fc74544f41a3
Matej Lukac <matej.lukac@tul.cz>
2025-01-25 18:24:32 +0100

    Debugging

--- app/device/models/difference_model.py
+++ app/device/models/difference_model.py
@@ -1,4 +1,6 @@
 from app.device.models.device_model import Device as DeviceModel
+from app.device.service import device_service as ds
+
 
 class DeviceDifference:
     def __init__(self, nb_device: DeviceModel, zb_device: DeviceModel, differences: tuple[list[str],list[str]]) -> None:
@@ -11,8 +13,8 @@ class DeviceDifference:
 
     def print_differences(self) -> str:
         txt_builder: str = ""
-        txt_builder += f"Netbox device: {self.nb_device.name}\n"
-        txt_builder += f"Zabbix device: {self.zb_device.name}\n"
+        txt_builder += f"Netbox device: {ds.print_device(self.nb_device)}\n"
+        txt_builder += f"Zabbix device: {ds.print_device(self.zb_device.name)}\n"
         txt_builder += "Differences:\n"
         for difference in self.differences[0]:
             txt_builder += f"  {difference}\n"
