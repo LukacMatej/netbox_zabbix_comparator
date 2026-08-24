568a1cde84a79080e0953dccafcca4329f5a79aa
Matěj Lukáč <m.lukac@netsystem.cz>
2026-08-24 11:29:20 +0200

    Parse templates/hostgroups from html fix

--- server.py
+++ server.py
@@ -84,9 +84,9 @@ def dict_to_device(data: dict) -> Device:
     return Device(
         name=data["name"],
         interfaces=[dict_to_interface(i) for i in data["interfaces"]],
-        hostgroup=str(data["hostgroup"]).strip().split(","),
+        hostgroup=str(data["hostgroup"]).split(", "),
         description=data["description"],
-        templates=str(data["templates"]).strip().split(","),
+        templates=str(data["templates"]).split(", "),
         status=data["status"],
     )
 
